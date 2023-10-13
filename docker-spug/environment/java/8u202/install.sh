d751075b4b661bddab57ab403adaa11d0a10ffab
huang6349 <252683858@qq.com>
2023-10-13 15:23:13 +0800

    :art: spug

---
+++ docker-spug/environment/java/8u202/install.sh
@@ -0,0 +1,4 @@
+curl -o jdk-8u202-linux-x64.tar.gz https://repo.huaweicloud.com/java/jdk/8u202-b08/jdk-8u202-linux-x64.tar.gz
+tar xf jdk-8u202-linux-x64.tar.gz -C /opt
+echo -e 'export JAVA_HOME=/opt/jdk1.8.0_202\nexport PATH=$PATH:$JAVA_HOME/bin' > /etc/profile.d/java.sh
+source /root/.bashrc
