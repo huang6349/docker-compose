abba15afdca6a1c38686a573b10a87a6912a422a
huang6349 <252683858@qq.com>
2023-10-13 10:58:13 +0800

    :art: mysql

--- docker-mysql/initdb.sql
+++ docker-mysql/initdb.sql
@@ -1,5 +1,2 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 -- noinspection SqlDialectInspectionForFile
-
-CREATE DATABASE IF NOT EXISTS `gogs` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE DATABASE IF NOT EXISTS `spug` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
